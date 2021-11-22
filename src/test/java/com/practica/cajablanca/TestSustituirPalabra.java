bbc7c74ba56bedb20d88a11b84ba41614abe29b9
vik0-bit <59226398+vik0-bit@users.noreply.github.com>
2021-11-22 11:18:30 +0100

    Add files via upload

---
+++ src/test/java/com/practica/cajablanca/TestSustituirPalabra.java
@@ -0,0 +1,61 @@
+package com.practica.cajanegra;
+
+import com.cajanegra.SingleLinkedListImpl;
+import  com.practica.cajablanca.Editor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/*
+CAMINOS:
+A,B,M
+A,B,C,D,E,J,K,L,M
+A,B,C,D,E,F,H,I,E,J,K,L,M
+A,B,C,D,E,F,G,I,E,J,K,L,M
+A,B,C,D,E,J,K,D,E,J,K,L,M
+*/
+/*
+public void sustituirPalabra(String palabra, String nuevaPalabra) {
+        if (this.editor.size() > 0) {
+        AbstractSingleLinkedListImpl<AbstractSingleLinkedListImpl<String>> nuevoEditor =
+        new SingleLinkedListImpl<AbstractSingleLinkedListImpl<String>>(); 3
+        int i=1;
+        do {
+        AbstractSingleLinkedListImpl<String> aux = new SingleLinkedListImpl<String>();
+        this.lista = this.editor.getAtPos(i);
+        int j=1;
+        while (j<=this.lista.size()) {
+        if (this.lista.getAtPos(j).equals(palabra)) {
+        aux.addLast(nuevaPalabra); ///Palabra encontrada
+        }else {
+        aux.addLast(this.lista.getAtPos(j)); //// primera palabrano buscada
+        }
+        j++;
+        }
+        nuevoEditor.addLast(aux);
+        i++;
+        }while (i<=this.editor.size());
+        editor = nuevoEditor;
+        }
+        }
+*/
+
+public class TestSustituirPalabra {
+    //Editor vacío
+    @Test
+    @DisplayName("CAMINO A,B,M")
+    public void sizeEditorSustituirPalabras(){
+        Editor editor = new Editor();
+        Editor esperado = editor;
+        assertEquals(esperado, editor);
+    }
+    //Cambiar segunda palabra
+    @Test
+    @DisplayName("CAMINO A,B,C,D,E,F,H,I,E,J,K,L,M")
+    public void size2EditorSustituirPalabras(toString("Piso"),toString("Arbol")){
+        
+        Editor editor = new Editor();
+        editor.leerFichero("Ficheros/SustituirPalabra.txt");
+        assertEquals("Casa Arbol", editor);
+    }
+
+}
